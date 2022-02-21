bb1ed4d787751ca87528f4a85cd4601abf36a0de
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-02-21 14:23:58 +0300

    Add project files.

---
+++ Mathematics.Tests/CalculationsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Tests
+{
+    [TestClass]
+    public class CalculationsTests
+    {
+
+        [TestMethod]
+        public void Test_Sum_Method()
+        {
+            Calculations calculations = new Calculations();
+            int result = calculations.Sum(7, 11);
+            Assert.AreEqual(18, result);
+
+        }
+
+        [TestMethod]
+        public void Test_Multiply_Method()
+        {
+            Calculations calculations = new Calculations();
+            int result = calculations.Multiply(7, 11);
+            Assert.AreEqual(77, result);
+        }
+
+    }
+}
